6686a2ae301e9c60f00954c8652d67c4af398b3b
Colby Fayock <fayock@gmail.com>
2024-08-02 22:19:57 -0400

    04 - Using Drizzle ORM to Write a new Database Schema

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial, text, timestamp, integer, pgEnum } from 'drizzle-orm/pg-core';
+
+export const statusEnum = pgEnum('status', ['open', 'paid', 'void', 'uncollectible']);
+
+export const Invoices = pgTable('invoices', {
+  id: serial('id').primaryKey().notNull(),
+  create_ts: timestamp('create_ts').defaultNow().notNull(),
+  description: text('description').notNull(),
+  status: statusEnum('status').notNull(),
+  value: integer('value').notNull(),
+});
