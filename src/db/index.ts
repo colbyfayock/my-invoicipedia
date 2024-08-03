140b1f8dd3e9abaf1579f4074cadf2a0de0edf41
Colby Fayock <fayock@gmail.com>
2024-08-02 22:45:38 -0400

    04 - Fix - connecting schema to db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
+import { Invoices } from './schema';
+
 const pool = new Pool({
   connectionString: process.env.XATA_DATABASE_URL
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, {
+  schema: { Invoices }
+});
