6561d3b7a2835842ab0c66a4bf2046aa3ced985d
Colby Fayock <fayock@gmail.com>
2024-08-06 22:01:55 -0400

    15 - Restricting Server Actions to Authenticated Users

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,12 +4,17 @@ import { redirect } from 'next/navigation';
 
 import { Invoices } from '@/db/schema';
 import { db } from '@/db';
+import { auth } from '@clerk/nextjs/server';
 
 /**
  * createInvoice
  */
 
 export async function createInvoice(formData: FormData) {
+  const { userId } = auth()
+
+  if ( !userId ) throw new Error('User not found');
+
   const description = formData.get('description') as string;
   const value = Math.floor(parseFloat(formData.get('value') as string) * 100);  
 
