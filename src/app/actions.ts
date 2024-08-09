2b6caa5576fb29d5fc34bacd43b73b759ef966dc
Colby Fayock <fayock@gmail.com>
2024-08-08 22:17:20 -0400

    FIX - remove customer creation from update

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -58,21 +58,8 @@ export async function updateStatus(formData: FormData) {
 
   if ( !userId ) throw new Error('User not found');
 
-  const name = formData.get('name') as string;
-  const email = formData.get('email') as string;
   const id = formData.get('id') as string;
   const status = formData.get('status') as Status;
-
-    // Create a customer
-
-  const customer = await db.insert(Customers)
-    .values({
-      name,
-      email,
-      user_id: userId,
-    }).returning({
-      id: Customers.id
-    });
 
   await db.update(Invoices)
     .set({ status })
